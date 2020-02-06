4e11904544d7e81ca7380011174d882251e1561a
hellomac87 <hellomac87@gmail.com>
2020-02-06 23:10:23 +0900

    connect mongodb

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://hellomac87:1234@youtube-clone-bebzw.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(e => console.log(e));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
