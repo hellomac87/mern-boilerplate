5aba999895368b8b335747cb219be30f564c8016
hellomac87 <hellomac87@gmail.com>
2020-02-08 20:32:31 +0900

    bcrypt 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const someOtherPlaintextPassword = "not_bacon";
 
 const userSchema = mongoose.Schema({
   name: {
@@ -28,6 +31,27 @@ const userSchema = mongoose.Schema({
   },
   tokenExp: {
     type: Number
+  }
+});
+
+// 유저 정보를 저장하기 전에
+userSchema.pre("save", function(next) {
+  var user = this; // <-- 유저스키마를 가르킴
+
+  // 패스워드가 변환될떄만
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화시긴다.
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
   }
 });
 
